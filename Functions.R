c0a84000bbdd16fbfb7b15fc2ddee5b68d1c6e57
samsiljee <samsiljee@gmail.com>
2025-07-14 07:34:04 +1200

    Added function to score row alignment

--- Functions.R
+++ Functions.R
@@ -2,20 +2,55 @@
 # Sam Siljee
 # 27th June 2025
 
-align_matrix <- function(image_matrix, max_x_offset) {
-  # Take the middle row as a reference row
-  reference_row <- image_matrix[round(nrow(image_matrix)/2),]
+# Function to get matrix offset values
+get_alignment_offsets <- function(image_matrix, max_offset, min_offset) {
+  # Set some variables
+  n_rows <- nrow(image_matrix)
   
-  # Define a function to check alignment
-  check_alignment <- function(image_row, reference_row, x_offset) {
-    # Create a row using the supplied offset
-    if(x_offset == 0) {
-      offset_row <- image_row
-    } else if (x_offset > 0) {
-      offset_row <- 
-    } else {
-      offset_row <- 
+  # Take the average of the middle fifth as a reference row
+  reference_row <- colMeans(image_matrix[round(n_rows*2/5):round(n_rows*3/5),])
+  
+  # Loop through rows
+  for(x in n_rows){
+    # Set image row
+    image_row <- image_matrix[x,]
+    
+    # Loop through offset values
+    for(o in min_offset:max_offset) {
+      score_alignment(image_row, reference_row, o)
     }
+    
   }
   
+}
+
+# Function to score row alignment
+score_alignment <- function(image_row, reference_row, x_offset) {
+  # Get original row length
+  row_length <- length(image_row)
+  
+  # Create a row using the supplied offset
+  if(x_offset == 0) {
+    offset_row <- image_row
+  } else if (x_offset > 0) {
+    # Trim the start of the row
+    offset_row <- image_row[(-x_offset+1):row_length]
+  } else {
+    # Trim the end of the row
+    offset_row <- image_row[1:(row_length-x_offset)]
+  }
+  
+  # Create a reference row of the same length
+  if(x_offset == 0) {
+    trimmed_reference_row <- reference_row
+  } else if (x_offset > 0) {
+    # Trim the end of the row
+    trimmed_reference_row <- reference_row[1:(row_length-x_offset)]
+  } else {
+    # Trim the start of the row
+    trimmed_reference_row <- reference_row[(-x_offset+1):row_length]
+  }
+  
+  # Return the mean absolute differences
+  return(mean(abs(trimmed_reference_row - offset_row)))
 }
